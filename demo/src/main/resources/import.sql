f34ed84834932eba37a67b5f8e4735cb875cf19d
Vasiliy_Miroshin <Vasiliy_Miroshin@epam.com>
2016-06-01 17:57:48 +0300

    DEMO-3 Hibernate initialization strategy

---
+++ demo/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO initialization_strategy VALUES (0, 'Spring JDBC Initialization', 1);
+INSERT INTO initialization_strategy VALUES (1, 'Spring Batch Initialization', 2);
+INSERT INTO initialization_strategy VALUES (2, 'Manual Initialization', 3);
+INSERT INTO initialization_strategy VALUES (3, 'Hibernate Initialization', 4);
+INSERT INTO initialization_strategy VALUES (4, 'Flyway Initialization', 5);
+INSERT INTO initialization_strategy VALUES (5, 'Liquidbase Initialization', 6);
+INSERT INTO initialization_strategy VALUES (6, 'Custom Entity Initialization', 7);
+INSERT INTO initialization_strategy VALUES (7, 'Maven SQL Plugin Initialization', 8);
+INSERT INTO initialization_strategy VALUES (99, 'Maven SQL Test Plugin Initialization', 8);
