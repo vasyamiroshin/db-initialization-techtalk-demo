4ce662cf9baa2c05fbb92132ae29a82c302523da
Vasiliy_Miroshin <Vasiliy_Miroshin@epam.com>
2016-06-02 15:34:13 +0300

    DEMO-5 Implementation of Flyway Initialization Strategy

---
+++ demo/src/main/java/com/epam/vm/techtalk/dbinitialization/configuration/BaselineOnMigrateMigrationStrategy.java
@@ -0,0 +1,16 @@
+package com.epam.vm.techtalk.dbinitialization.configuration;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaselineOnMigrateMigrationStrategy implements FlywayMigrationStrategy {
+	
+	@Override
+	public void migrate(Flyway flyway) {
+		flyway.setBaselineOnMigrate(true);
+		flyway.migrate();
+	}
+	
+}
